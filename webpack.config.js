ea5b9479b3febc5f444d6dfed8ac118485147152
MyCandy160 <3087027139@qq.com>
2022-06-17 20:37:36 +0800

    wbpack serve

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,21 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { join } = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: join(__dirname, "dist"),
     filename: "bundle.js",
+    clean: true,
   },
+  mode: "production",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+  ],
+  devServer: {
+    port: 2002, // 端口号
+    open: true
+  }
 };
