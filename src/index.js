b65fe93a604bc8f64340a3e7557b2be3507757b3
MyCandy160 <3087027139@qq.com>
2022-06-17 21:25:37 +0800

    imgiconfonts

--- src/index.js
+++ src/index.js
@@ -2,4 +2,14 @@ import { addfn } from "./app/app.js";
 import "./style/index.css";
 import "./style/index.less";
 
+import image from "./assets/1.gif";
+import image2 from "./assets/logo_small.png";
+const img = document.createElement("img");
+const img2 = document.createElement("img");
+img.src = image;
+img2.src = image2;
+document.body.appendChild(img);
+document.body.appendChild(img2);
+import './assets/fonts/iconfont.css'
+
 console.log(addfn(1));
--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,20 @@ module.exports = {
         test: /\.less$/i,
         use: ["style-loader", "css-loader", "less-loader"],
       },
+      {
+        test: /\.(png|gif|jpg|jpeg)$/i,
+        type: "asset",
+        generator: { filename: "image/[hash:6][ext]" },
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/i,
+        type: "asset/resource",
+        generator: { filename: "fonts-[name].[hash:6][ext]" },
+      },
+      {
+        test: /\.js$/i,
+        use: ["babel-loader"],
+      },
     ],
   },
 };
